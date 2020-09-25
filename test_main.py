bcde8dfff48020552cd1fb4b6907a58e52ed0a65
rachelle <rachelle@modopayments.com>
2020-09-25 05:50:40 -0500

    testing out unit test failures

--- test_main.py
+++ test_main.py
@@ -14,6 +14,7 @@ PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
+	assert False
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
